c5a84b9a31bbde2add2aef3a97e33477a8710a20
Abhijeet <abhijeet.millenium@gmail.com>
2020-04-29 19:10:56 -0600

    recipe app updated

--- src/main/java/guru/springframework/bootstrap/RecipeBootstrap.java
+++ src/main/java/guru/springframework/bootstrap/RecipeBootstrap.java
@@ -4,6 +4,7 @@ import guru.springframework.domain.*;
 import guru.springframework.repositories.CategoryRepository;
 import guru.springframework.repositories.RecipeRepository;
 import guru.springframework.repositories.UnitOfMeasureRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -13,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Component
 public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
@@ -29,6 +31,7 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
         recipeRepository.saveAll(getRecipe());
+        log.debug("Loading Bootstrap Data");
     }
 
     public List<Recipe> getRecipe(){
--- src/main/java/guru/springframework/controllers/indexController.java
+++ src/main/java/guru/springframework/controllers/indexController.java
@@ -2,10 +2,12 @@ package guru.springframework.controllers;
 
 
 import guru.springframework.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 public class indexController {
 
@@ -17,7 +19,7 @@ public class indexController {
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(Model model){
-
+        log.debug("Getting Index Page");
         model.addAttribute("recipes", recipeService.getRecipe());
         return "index";
     }
