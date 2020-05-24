1553c6ff66409207ff1aee9eaca4add33b436b09
Abhijeet <abhijeet.millenium@gmail.com>
2020-05-23 21:10:42 -0600

    Added id number which is not found on 404 html error page

--- src/main/java/guru/springframework/services/RecipeServiceImpl.java
+++ src/main/java/guru/springframework/services/RecipeServiceImpl.java
@@ -44,7 +44,7 @@ public class RecipeServiceImpl implements RecipeService {
 
         if (!recipeOptional.isPresent()) {
             // throw new RuntimeException("Recipe Not Found!");
-            throw new NotFoundException("Recipe Not Found!");
+            throw new NotFoundException("Recipe Not Found for id value: " + id.toString());
         }
 
         return recipeOptional.get();
